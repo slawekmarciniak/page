0ff33ca320f757ce7e3eb715256a14c5f2d9087d
Slawek <61154596+Ultran@users.noreply.github.com>
2021-04-15 00:45:00 +0200

    add new colors and change same  fonts datails

--- js/mainSection.js
+++ js/mainSection.js
@@ -4,9 +4,9 @@ export function mainSection() {
     scrollTrigger: {
       trigger: ".main",
       start: "top 300px",
-      end: "top 50px",
+      end: "top 75px",
       scrub: 1,
-      markers: true,
+      // markers: true,
     },
   });
 
@@ -15,7 +15,17 @@ export function mainSection() {
     scrollTrigger: {
       trigger: ".main",
       start: "top 200px",
-      end: "top 10px",
+      end: "top 50px",
+      scrub: 1,
+    },
+  });
+
+  gsap.from(".offer", {
+    fontSize: "0.2",
+    scrollTrigger: {
+      trigger: ".main",
+      start: "top 400px",
+      end: "top 200px",
       scrub: 1,
     },
   });
--- js/technologies.js
+++ js/technologies.js
@@ -1,1 +1,33 @@
-export function technologies() {}
+export function technologies() {
+  gsap.from(".technologieImageA", {
+    x: -1000,
+    rotation: "-1080",
+    scrollTrigger: {
+      trigger: ".technologies",
+      start: "top 300px",
+      end: "top 50px",
+      scrub: 1,
+      markers: true,
+    },
+  });
+
+  gsap.from(".technologieImageB", {
+    x: 1000,
+    scrollTrigger: {
+      trigger: ".technologies",
+      start: "top 200px",
+      end: "top 10px",
+      scrub: 1,
+    },
+  });
+
+  gsap.from(".technologieHeader", {
+    fontSize: "0.2",
+    scrollTrigger: {
+      trigger: ".technologies",
+      start: "top 400px",
+      end: "top 200px",
+      scrub: 1,
+    },
+  });
+}
